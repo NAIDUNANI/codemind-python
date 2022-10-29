8812ca6b00773c7c6837f79a2acb5ed1e89b5a7c
NAIDUNANI <84012731+NAIDUNANI@users.noreply.github.com>
2022-10-29 10:12:08 +0530

    def pali(n):     rev=0     while n>0:         r=n%10         rev=rev*10+r         n=n//10     return rev a=int(input()) if pali(a)==a:     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,13 @@
-n=int(input())
-m=n
-rev=0
-while n>0:
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-if rev==m:
+def pali(n):
+    rev=0
+    while n>0:
+        r=n%10
+        rev=rev*10+r
+        n=n//10
+    return rev
+a=int(input())
+if pali(a)==a:
     print("True")
 else:
     print("False")
+        
