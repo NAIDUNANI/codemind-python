ef22b2c32343c692e7bc196448fd41c834b25391
NAIDUNANI <84012731+NAIDUNANI@users.noreply.github.com>
2022-10-29 09:57:04 +0530

    def pfs(num):     fs=0     for i in range(1,num):         if num % i==0:             fs += i     return fs a=int(input()) b=int(input()) if(pfs(b) == a and pfs(a) == b):     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,15 +1,12 @@
+def pfs(num):
+    fs=0
+    for i in range(1,num):
+        if num % i==0:
+            fs += i
+    return fs
 a=int(input())
 b=int(input())
-sum1=0
-sum2=0
-for i in range(1,a):
-    if a%i==0:
-        sum1+=i
-for j in range(1,b):
-    if b%j==0:
-        sum2+=j
-if sum1==b and sum2==a:
+if(pfs(b) == a and pfs(a) == b):
     print("Amicable")
 else:
     print("Not Amicable")
-        
